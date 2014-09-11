d55877f6bae1a84fe3e3eb34a55a49f5a06e6753
Marco Sciatta <marco.sciatta@gmail.com>
2014-09-11 21:37:40 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Includiamo il modulo express
 var express = require('express');
+var logger = require('morgan');
 
 //Creiamo la nostra applicaizone
 var app = express();
@@ -9,6 +10,18 @@ var port = process.env.PORT || 8080;
 
 //Otteniamo una istanza del router 
 var router = express.Router();
+
+
+//Aggiungiamo un logger
+if('development' == app.get('env')){
+	app.use(logger('dev'));
+}
+
+//Aggiungiamo un piccolo middleware
+app.use(function(req, res, next) {
+  console.log('%s %s from %s', req.method, req.url , req.ip);
+  next();
+});
 
 
 //Definiamo una rotta di benvenuto 
@@ -23,7 +36,7 @@ router.get('/',function(req,res){
 });
 
 //Colleghiamo le rotte contenute in router all'url /api
-app.use('/api',router);
+app.use('/api/v1',router);
 
 //Mettiamo il server in ascolto
 app.listen(port);
